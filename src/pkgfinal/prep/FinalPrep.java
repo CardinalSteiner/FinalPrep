b2e3727827f1b745f18d135497067a1b8f044558
CardinalSteiner <ranaashu@sheridancollege.ca>
2020-08-10 23:36:29 -0400

    first Commit

---
+++ src/pkgfinal/prep/FinalPrep.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal.prep;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cardinal Steiner
+ */
+public class FinalPrep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type a word");
+        String x = input.next();
+        char[] y = new char[x.length()];
+        for(int i = 0; i< x.length(); i++){
+        y[i] = x.charAt(i);
+        }
+        char[] z = new char[x.length()];
+        for(int i = 0 ; i< x.length(); i++){
+        z[i] = y[x.length()-1-i];
+        }
+        
+        System.out.println(z);
+        
+        
+        
+        
+    }
+    
+}
